55da2fe20fa67fb768ad68554973bb7a7c5f48cc
faseed <mohamedfaseeda@gmail.com>
2026-01-10 11:59:02 +0530

    alignment 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import './globals.css';
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
